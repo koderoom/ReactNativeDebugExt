1e5b45242bb2c6040899da351caf49b0096de753
santosh <santosh.ece06@gmail.com>
2018-11-29 21:34:58 +0530

    Source Code Ready

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 
 export default class App extends Component {
@@ -8,13 +8,28 @@ export default class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = {};
+    let bglist = ['green',  'royalblue', 'tomato',  '#f44336', '#e91e63', '#9c27b0', '#2196f3', '#8bc34a', '#607d8b'];
+    this.state = {'title' : 'Hello Native', 'bglist' : bglist, 'styles':styles, 'bgfy': styles.bgfy};
+
+    this.onClickChangeBackground = this.onClickChangeBackground.bind(this);
+  }
+
+
+  onClickChangeBackground() {
+      const newstyles = JSON.parse(JSON.stringify(styles));
+      const random = Math.floor(Math.random() * this.state.bglist.length);
+
+      newstyles.bgfy.backgroundColor = this.state.bglist[random];
+      const newstate = {'bgfy': newstyles.bgfy};
+
+      this.setState(newstate);
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <View style={[styles.container, this.state.bgfy]}>
+          <Text style={styles.textify}>{this.state.title}</Text>
+          <Button title='Click Here' color={this.state.bgfy.backgroundColor} onPress={this.onClickChangeBackground}></Button>
       </View>
     );
   }
@@ -25,16 +40,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
+  textify: {
+    fontSize: 50,
+    color:'white',
+    fontFamily : 'cursive'
+    
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+
+  bgfy : {
+    backgroundColor: 'tomato'
   },
 });
